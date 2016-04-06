eaa5848187ba68f2a4dba97de440e3c312532166
João Bernardes <joao-lb@bol.com.br>
2016-04-06 11:48:33 -0300

    Update chkAddress.sh

--- chkAddress.sh
+++ chkAddress.sh
@@ -7,7 +7,7 @@ checkFile="/var/log/addressStatus/lastAddress"
 MAIL="./sendEmail.sh"
 currentAddress=`dig +short myip.opendns.com @resolver1.opendns.com`
 lastAddress=`cat $checkFile`
-MONADDR="example@gmail.com"
+MONADDR="receiver@gmail.com"
 DATE=`date +%d-%m-%Y`
 TIME=`date +%H%M`
 
@@ -15,7 +15,7 @@ if [ "$currentAddress" != "$lastAddress" ]; then
    echo "The IP Address has been changed! New Address: $currentAddress"
    echo $currentAddress > $checkFile
    $MAIL $MONADDR "Alteração de Endereço IP" \
-      "O endereço IP do ambiente monitorado foi alterado para $currentAddress \n
+      "O endereço IP publico do ambiente monitorado foi alterado para $currentAddress \n
        Informação coletada em $DATE, às $TIME"
 else
    echo "The IP Address stay the same: $currentAddress" >> /var/log/chkAddress.log
